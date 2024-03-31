9cefbd50df8b38a42c9afb6f494ac4c4e0a48f80
Kemal Fadillah <fadillzzz@gmail.com>
2024-03-31 01:21:48 -0700

    Adds define guards

--- kiero.h
+++ kiero.h
@@ -5,23 +5,43 @@
 
 #define KIERO_VERSION "1.2.12"
 
-#define KIERO_INCLUDE_D3D9   0 // 1 if you need D3D9 hook
-#define KIERO_INCLUDE_D3D10  0 // 1 if you need D3D10 hook
-#define KIERO_INCLUDE_D3D11  0 // 1 if you need D3D11 hook
-#define KIERO_INCLUDE_D3D12  0 // 1 if you need D3D12 hook
+#ifndef KIERO_INCLUDE_D3D9
+#define KIERO_INCLUDE_D3D9 0 // 1 if you need D3D9 hook
+#endif
+
+#ifndef KIERO_INCLUDE_D3D10
+#define KIERO_INCLUDE_D3D10 0 // 1 if you need D3D10 hook
+#endif
+
+#ifndef KIERO_INCLUDE_D3D11
+#define KIERO_INCLUDE_D3D11 0 // 1 if you need D3D11 hook
+#endif
+
+#ifndef KIERO_INCLUDE_D3D12
+#define KIERO_INCLUDE_D3D12 0 // 1 if you need D3D12 hook
+#endif
+
+#ifndef KIERO_INCLUDE_OPENGL
 #define KIERO_INCLUDE_OPENGL 0 // 1 if you need OpenGL hook
+#endif
+
+#ifndef KIERO_INCLUDE_VULKAN
 #define KIERO_INCLUDE_VULKAN 0 // 1 if you need Vulkan hook
-#define KIERO_USE_MINHOOK    0 // 1 if you will use kiero::bind function
+#endif
+
+#ifndef KIERO_USE_MINHOOK
+#define KIERO_USE_MINHOOK 0 // 1 if you will use kiero::bind function
+#endif
 
 #define KIERO_ARCH_X64 0
 #define KIERO_ARCH_X86 0
 
-#if defined(_M_X64)	
-# undef  KIERO_ARCH_X64
-# define KIERO_ARCH_X64 1
+#if defined(_M_X64)
+#undef KIERO_ARCH_X64
+#define KIERO_ARCH_X64 1
 #else
-# undef  KIERO_ARCH_X86
-# define KIERO_ARCH_X86 1
+#undef KIERO_ARCH_X86
+#define KIERO_ARCH_X86 1
 #endif
 
 #if KIERO_ARCH_X64
@@ -30,49 +50,44 @@ typedef uint64_t uint150_t;
 typedef uint32_t uint150_t;
 #endif
 
-namespace kiero
-{
-	struct Status
-	{
-		enum Enum
-		{
-			UnknownError = -1,
-			NotSupportedError = -2,
-			ModuleNotFoundError = -3,
+namespace kiero {
+    struct Status {
+        enum Enum {
+            UnknownError = -1,
+            NotSupportedError = -2,
+            ModuleNotFoundError = -3,
 
-			AlreadyInitializedError = -4,
-			NotInitializedError = -5,
+            AlreadyInitializedError = -4,
+            NotInitializedError = -5,
 
-			Success = 0,
-		};
-	};
+            Success = 0,
+        };
+    };
 
-	struct RenderType
-	{
-		enum Enum
-		{
-			None,
+    struct RenderType {
+        enum Enum {
+            None,
 
-			D3D9,
-			D3D10,
-			D3D11,
-			D3D12,
+            D3D9,
+            D3D10,
+            D3D11,
+            D3D12,
 
-			OpenGL,
-			Vulkan,
+            OpenGL,
+            Vulkan,
 
-			Auto
-		};
-	};
+            Auto
+        };
+    };
 
-	Status::Enum init(RenderType::Enum renderType);
-	void shutdown();
+    Status::Enum init(RenderType::Enum renderType);
+    void shutdown();
 
-	Status::Enum bind(uint16_t index, void** original, void* function);
-	void unbind(uint16_t index);
+    Status::Enum bind(uint16_t index, void **original, void *function);
+    void unbind(uint16_t index);
 
-	RenderType::Enum getRenderType();
-	uint150_t* getMethodsTable();
-}
+    RenderType::Enum getRenderType();
+    uint150_t *getMethodsTable();
+} // namespace kiero
 
 #endif // __KIERO_H__
